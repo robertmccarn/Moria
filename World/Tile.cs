8c69b5bfb83e383adb8f65ba0a2e14fba96ed51d
robertmccarn <54292691+robertmccarn@users.noreply.github.com>
2026-09-15 16:04:29 -0500

    Add dungeon tile model

---
+++ World/Tile.cs
@@ -0,0 +1,22 @@
+namespace Moria.World;
+
+public enum TileType { Rock, Floor, Wall, Door, StairsUp, StairsDown, Trap }
+
+public sealed class Tile
+{
+    public TileType Type { get; set; } = TileType.Rock;
+    public bool Lit { get; set; }
+    public bool Seen { get; set; }
+    public bool HasItem { get; set; }
+
+    public char Symbol => Type switch
+    {
+        TileType.Floor => '.',
+        TileType.Wall => '#',
+        TileType.Door => '+',
+        TileType.StairsUp => '<',
+        TileType.StairsDown => '>',
+        TileType.Trap => '^',
+        _ => '#'
+    };
+}
