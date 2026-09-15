689d0fb42829b65d1333c540fd4601e9af7c10a1
robertmccarn <54292691+robertmccarn@users.noreply.github.com>
2026-09-15 16:30:56 -0500

    Add gear loot to dungeon tiles

--- World/Tile.cs
+++ World/Tile.cs
@@ -1,3 +1,5 @@
+using Moria.Items;
+
 namespace Moria.World;
 
 public enum TileType { Rock, Floor, Wall, Door, StairsUp, StairsDown, Trap }
@@ -8,6 +10,7 @@ public sealed class Tile
     public bool Lit { get; set; }
     public bool Seen { get; set; }
     public bool HasItem { get; set; }
+    public Gear? Gear { get; set; }
 
     public char Symbol => Type switch
     {
