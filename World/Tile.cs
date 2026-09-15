6914739f60255e7f169de264401a51be706ac4ad
robertmccarn <54292691+robertmccarn@users.noreply.github.com>
2026-09-15 16:46:59 -0500

    Allow multiple loot drops per tile

--- World/Tile.cs
+++ World/Tile.cs
@@ -9,8 +9,9 @@ public sealed class Tile
     public TileType Type { get; set; } = TileType.Rock;
     public bool Lit { get; set; }
     public bool Seen { get; set; }
-    public bool HasItem { get; set; }
-    public Gear? Gear { get; set; }
+    public bool HasItem => PotionCount > 0;
+    public int PotionCount { get; set; }
+    public List<Gear> GearLoot { get; } = new();
 
     public char Symbol => Type switch
     {
