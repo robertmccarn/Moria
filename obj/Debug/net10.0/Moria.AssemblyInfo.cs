34f619fe62b84a30c9b2fdeed0665cc25707130a
Robert McCarn <robertmccarn@gmail.com>
2026-09-15 17:19:09 -0500

    Assets

--- obj/Debug/net10.0/Moria.AssemblyInfo.cs
+++ obj/Debug/net10.0/Moria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Moria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98ddf6a6e99b480398c88e9f9db49c0443210dc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Moria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Moria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
