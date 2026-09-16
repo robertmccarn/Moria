081016d55e84a68bb4c1d43695e4976aa96f9d68
robertmccarn <54292691+robertmccarn@users.noreply.github.com>
2026-09-15 21:43:29 -0500

    Centralize retro UI palette

---
+++ UI/UiTheme.cs
@@ -0,0 +1,19 @@
+using System.Drawing;
+
+namespace Moria.UI;
+
+public static class UiTheme
+{
+    public static readonly Color Background = Color.FromArgb(8, 10, 15);
+    public static readonly Color Panel = Color.FromArgb(18, 21, 29);
+    public static readonly Color PanelInset = Color.FromArgb(13, 16, 23);
+    public static readonly Color Border = Color.FromArgb(61, 67, 80);
+    public static readonly Color Gold = Color.FromArgb(220, 177, 68);
+    public static readonly Color Text = Color.FromArgb(232, 231, 222);
+    public static readonly Color Muted = Color.FromArgb(145, 151, 163);
+    public static readonly Color Hp = Color.FromArgb(190, 68, 61);
+    public static readonly Color Xp = Color.FromArgb(77, 137, 205);
+    public static readonly Color Good = Color.FromArgb(91, 181, 113);
+    public static readonly Color Accent = Color.FromArgb(104, 184, 207);
+    public static readonly Color Negative = Color.FromArgb(194, 70, 65);
+}
