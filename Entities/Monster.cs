054544213fa1f2485a2c092ff08fcad001d25a2c
robertmccarn <54292691+robertmccarn@users.noreply.github.com>
2026-09-15 21:11:27 -0500

    Add boss monster identity

--- Entities/Monster.cs
+++ Entities/Monster.cs
@@ -6,10 +6,12 @@ public sealed class Monster : Actor
 {
     public int ExperienceValue { get; }
     public bool Hostile { get; set; } = true;
+    public bool IsBoss { get; }
 
-    public Monster(string name, char symbol, Position position, int hp, int armorClass, int attack, int level, int experienceValue)
+    public Monster(string name, char symbol, Position position, int hp, int armorClass, int attack, int level, int experienceValue, bool isBoss = false)
         : base(name, symbol, position, hp, armorClass, attack, level)
     {
         ExperienceValue = experienceValue;
+        IsBoss = isBoss;
     }
 }
