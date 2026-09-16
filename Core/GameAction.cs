b4f1c6395f6599f1d1808cdd2b3d807864527d98
robertmccarn <54292691+robertmccarn@users.noreply.github.com>
2026-09-15 21:42:27 -0500

    Add game action abstraction

---
+++ Core/GameAction.cs
@@ -0,0 +1,16 @@
+namespace Moria.Core;
+
+public enum GameAction
+{
+    None,
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+    DrinkPotion,
+    Interact,
+    ChatLog,
+    Pause,
+    Quit,
+    NewRun
+}
